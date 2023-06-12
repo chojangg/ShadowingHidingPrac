3a3c4ec47aabd1d91be8cbf6b82ab66a3ec4139e
chojangg <s2115@e-mirim.hs.kr>
2023-06-12 13:31:06 +0900

    Shadowing 예제

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -8,15 +8,11 @@ namespace ShadowingHidingPrac
 {
     internal static class Program
     {
-        /// <summary>
-        /// 해당 애플리케이션의 주 진입점입니다.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public static int number = 10;
+        static void Main(String[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int number = 20;
+            Console.WriteLine(number);
         }
     }
 }
