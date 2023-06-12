be3078f78de3251fd4bf912c61ff07434f2bd008
chojangg <s2115@e-mirim.hs.kr>
2023-06-12 14:15:18 +0900

    하이딩과 오버라이딩 차이를 활용한 실습

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -8,6 +8,28 @@ namespace ShadowingHidingPrac
 {
     internal static class Program
     {
+        class Animal
+        {
+            public virtual void Eat()
+            {
+                Console.WriteLine("냠냠 먹습니다");
+            }
+        }
+        class Dog : Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("참참 먹습니다.");
+            }
+        }
+        class Cat : Animal
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("옴뇸 먹습니다");
+            }
+        }
+
         class Parent
         {
             public int variable = 273;
@@ -49,6 +71,16 @@ namespace ShadowingHidingPrac
 
             child.Method2();
             p.Method2();
+
+            List<Animal> Animals = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Cat(), new Dog(),
+                new Dog(), new Cat(), new Dog(), new Dog(),
+            };
+            foreach (var item in Animals)
+            {
+                item.Eat();
+            }
         }
     }
 }
